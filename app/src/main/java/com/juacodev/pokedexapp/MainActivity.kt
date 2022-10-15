abdf989faed66251461dc087c9f22dc38db93a95
juacosoftdev <joaquinalfonso.martinez@gmail.com>
2022-10-15 09:45:55 -0500

    initial commit

---
+++ app/src/main/java/com/juacodev/pokedexapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.juacodev.pokedexapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juacodev.pokedexapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/juacodev/pokedexapp/PokedexAppication.kt
@@ -0,0 +1,8 @@
+package com.juacodev.pokedexapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexAppication:Application() {
+}
